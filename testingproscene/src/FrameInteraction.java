f095851961559fec8f6f1bffd409463756d06496
nakednous <nakednous@727c4ca2-1e5a-11df-b3f3-c78161b9101f>
2010-03-14 23:12:31 +0000

    renaming moveable frame example to frame interaction and improving it

--- testingproscene/src/FrameInteraction.java
+++ testingproscene/src/FrameInteraction.java
@@ -3,42 +3,47 @@ import processing.opengl.*;
 import remixlab.proscene.*;
 
 @SuppressWarnings("serial")
-public class MovableFrame extends PApplet {
+public class FrameInteraction extends PApplet {
 	Scene scene;
 	
 	public void setup()	{
 		size(640, 360, OPENGL);
 		scene = new Scene(this); 
 		scene.setCameraType(Camera.Type.ORTHOGRAPHIC);
-		scene.setRadius(scene.radius()*1.3f);
-		scene.showAll();
 		scene.setGridIsDrawn(true);
 		scene.setAxisIsDrawn(true);		
 		scene.setInteractiveFrame(new InteractiveFrame());
-		scene.interactiveFrame().translate(new PVector(0.2f, 0.2f, 0));
-		scene.setDrawInteractiveFrame(true);
+		scene.interactiveFrame().translate(new PVector(0.3f, 0.3f, 0));
 	}
 
 	public void draw() {
 		background(0);
-		//set camera stuff, always necessary:
+		// set camera stuff, always necessary:
 		scene.beginDraw();
 		// Here we are in the world coordinate system.
-		// Draw your scene here.
-		//background(0);
+		// Draw your scene here.		
 		fill(204, 102, 0);
-		box(0.2f, 0.3f, 0.5f);
+		box(0.2f, 0.3f, 0.4f);
 		// Save the current model view matrix
 		pushMatrix();
 		// Multiply matrix to get in the frame coordinate system.
-		//applyMatrix( scene.interactiveFrame().pMatrix() );
-		//Same as the previous commented line, but a lot more efficient:
-		scene.interactiveFrame().applyTransformation(this);
-		// Draw an axis using the GLScene static function
-		Scene.drawAxis();
+		// applyMatrix(scene.interactiveFrame().matrix()) is possible but inefficient 
+		scene.interactiveFrame().applyTransformation(this);//very efficient
+		// Draw an axis using the Scene static function
+		Scene.drawAxis(0.7f);
 		// Draw a second box
-		fill(255, 0, 0);
-		box(0.1f, 0.15f, 0.25f);
+		if (scene.interactiveFrame().grabsMouse()) {
+			fill(255, 0, 0);
+			box(0.12f, 0.17f, 0.22f);
+		}
+		else if (scene.interactiveFrameIsDrawn()) {
+			fill(0, 255, 255);
+			box(0.12f, 0.17f, 0.22f);
+		}
+		else {
+			fill(0,0,255);
+			box(0.1f, 0.15f, 0.2f);
+		}		
 		popMatrix();
 		scene.endDraw();
 	}	
@@ -48,6 +53,6 @@ public class MovableFrame extends PApplet {
 	}
 	
 	public static void main(String args[]) {
-		PApplet.main(new String[] { "--present", "test.MovableFrame" });
+		PApplet.main(new String[] { "--present", "test.FrameInteraction" });
 	}
 }
