098b58d857ff32eb52196297c494f3f228c59985
nakednous <nakednous@727c4ca2-1e5a-11df-b3f3-c78161b9101f>
2010-08-30 20:28:35 +0000

    basic profile layout

---
+++ src/remixlab/proscene/ArcballProfile.java
@@ -0,0 +1,14 @@
+package remixlab.proscene;
+
+public class ArcballProfile extends CameraProfile {
+	/**
+	enum KeyboardAction { INCREASE_ROTATION_SENSITIVITY, DECREASE_ROTATION_SENSITIVITY 
+	}
+	*/
+
+	public ArcballProfile(Scene scn) {
+		super(scn);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/remixlab/proscene/FirstPersonProfile.java
@@ -0,0 +1,12 @@
+package remixlab.proscene;
+
+public class FirstPersonProfile extends CameraProfile {
+	
+	//enum KeyboardAction { INCREASE_FLYSPEED, DECREASE_FLYSPEED }
+
+	public FirstPersonProfile(Scene scn) {
+		super(scn);
+		// TODO Auto-generated constructor stub
+	}
+
+}
