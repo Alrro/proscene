8a6c9cc8e2e1c655ccff37ac31e403dd23c4050b
nakednous <nakednous@727c4ca2-1e5a-11df-b3f3-c78161b9101f>
2011-01-10 17:17:29 +0000

    adding execution modes to KeyFrameInterpolator

--- trunk/src/remixlab/proscene/Scene.java
+++ trunk/src/remixlab/proscene/Scene.java
@@ -1,5 +1,5 @@
 /**
- *                     ProScene (version 1.0.0)      
+ *                     ProScene (version 1.0.1)      
  *        Copyright (c) 2010 by National University of Colombia
  *                 @author Jean Pierre Charalambos      
  *            http://www.disi.unal.edu.co/grupos/remixlab/
@@ -100,7 +100,7 @@ import java.util.TimerTask;
  */
 public class Scene implements PConstants {
 	// proscene version
-	public static final String version = "1.0.0";
+	public static final String version = "1.0.1";
 	/**
 	 * Returns the major release version number of proscene as an integer.
 	 * <p>
