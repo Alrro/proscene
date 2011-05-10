7f9506a851266f76274bebf8e79c507aa5dcfca8
nakednous <nakednous@727c4ca2-1e5a-11df-b3f3-c78161b9101f>
2011-05-10 20:56:59 +0000

    nesting cone class into camera class

--- src/remixlab/proscene/Camera.java
+++ src/remixlab/proscene/Camera.java
@@ -56,6 +56,80 @@ import java.util.Iterator;
  */
 public class Camera implements Cloneable {
 	/**
+	 * Internal class that represents/holds a cone of normals.
+	 * Typically needed to perform bfc.
+	 */
+	public class Cone {
+		PVector axis;
+		float angle;
+		
+		public Cone() {
+			reset();
+		}
+		
+		public Cone(PVector vec, float a) {
+			set(vec, a);
+		}
+		
+		public Cone(ArrayList<PVector> normals) {
+			set(normals);
+		}
+		
+		public Cone(PVector [] normals) {
+			set(normals);
+		}
+		
+		public PVector axis() {
+			return axis;
+		}
+		
+		public float angle() {
+			return angle;
+		}
+		
+		public void reset() {
+			axis = new PVector(0,0,1);
+			angle = 0;
+		}
+		
+		public void set(PVector vec, float a) {
+			axis = vec;
+			angle = a;
+		}
+		
+		public void set(ArrayList<PVector> normals) {
+			set( normals.toArray( new PVector [normals.size()] ) );		
+		}
+		
+		public void set(PVector [] normals) {
+			axis = new PVector(0,0,0);
+			if(normals.length == 0) {
+				reset();
+				return;
+			}
+			
+			PVector [] n = new PVector [normals.length];
+			for(int i=0; i<normals.length; i++ ) {
+				n[i] = new PVector();
+				n[i].set(normals[i]);
+				n[i].normalize();
+				axis = PVector.add(axis, n[i]);
+			}
+			
+			if ( axis.mag() != 0 ) {
+	      axis.normalize();
+	    }
+	    else {
+	      axis.set(0,0,1);
+	    }
+			
+			angle = 0;        
+			for(int i=0; i<normals.length; i++ )		
+				angle = PApplet.max( angle, PApplet.acos( PVector.dot(n[i], axis)));		
+		}	
+	}
+	
+	/**
 	 * Internal class provided to catch the output of
 	 * {@link remixlab.proscene.Camera#pointUnderPixel(Point)} (which should be
 	 * implemented by an openGL based derived class Camera).
@@ -1464,7 +1538,7 @@ public class Camera implements Cloneable {
 	protected boolean frustumEquationsUpdateIsEnable() {
 		return fpCoefficientsUpdate;
 	}
-	
+		
 	/**
 	 * Convenience function that simply calls {@code coneIsBackFacing(new Cone(normals))}.
 	 * 
