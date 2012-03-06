0487490621891cc410e35a932684b3f8c6ede91f
nakednous <nakednous@727c4ca2-1e5a-11df-b3f3-c78161b9101f>
2012-03-06 23:50:04 +0000

    various fixes to the timers that allow switching between types of timers on run time

--- src/remixlab/proscene/TimerWrap.java
+++ src/remixlab/proscene/TimerWrap.java
@@ -9,16 +9,23 @@ public class TimerWrap implements Timable {
 	Timer timer;
 	TimerTask timerTask;
 	Taskable caller;
-	
+	boolean runOnlyOnce;
+	long prd;
+
 	public TimerWrap(Scene scn, Taskable o) {
+		this(scn, o, false);
+	}
+
+	public TimerWrap(Scene scn, Taskable o, boolean singleShot) {
 		scene = scn;
+		runOnlyOnce = singleShot;
 		caller = o;
-	}	
-	
+	}
+
 	public Taskable timerJob() {
 		return caller;
 	}
-	
+
 	@Override
 	public void create() {
 		stop();
@@ -29,34 +36,60 @@ public class TimerWrap implements Timable {
 			}
 		};
 	}
-  
+
 	@Override
-  public void run(long period) {
-  	create();
-		timer.scheduleAtFixedRate(timerTask, 0, period);
-  }
-  
-  @Override
-  public void runOnce(long period) {
-  	create();
-		timer.schedule(timerTask, period);
-  }
-  
-  @Override
-  public void cancel() {
-  	stop();
-  }
-  
-  @Override
-  public void stop() {
-  	if(timer != null) {
+	public void run(long period) {
+		prd = period;
+		run();
+	}
+
+	@Override
+	public void run() {
+		create();
+		if(isSingleShot())
+			timer.schedule(timerTask, prd);
+		else
+			timer.scheduleAtFixedRate(timerTask, 0, prd);		
+	}
+
+	@Override
+	public void cancel() {
+		stop();
+	}
+
+	@Override
+	public void stop() {
+		if (timer != null) {
 			timer.cancel();
 			timer.purge();
+			/**
+			 * prd = 0; runOnlyOnce = false;
+			 */
 		}
-  }
+	}
 
 	@Override
 	public boolean isActive() {
 		return timer != null;
+	}
+
+	@Override
+	public long period() {
+		return prd;
+	}
+
+	@Override
+	public void setPeriod(long period) {
+		prd = period;
+	}
+
+	@Override
+	public boolean isSingleShot() {
+		return runOnlyOnce;
+	}
+
+	@Override
+	public void setSingleShot(boolean singleShot) {
+		runOnlyOnce = singleShot;
 	}
 }
