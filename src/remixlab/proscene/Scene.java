a6d0b638e57640cbd84cd5b234315a9a47c87dad
nakednous <nakednous@gmail.com>
2012-08-07 23:07:58 +0000

    to release 1.1.91 aka belo horizonte release

--- src/remixlab/proscene/Scene.java
+++ src/remixlab/proscene/Scene.java
@@ -1,5 +1,5 @@
 /**
- *                     ProScene (version 1.1.90)      
+ *                     ProScene (version 1.1.91)      
  *    Copyright (c) 2010-2012 by National University of Colombia
  *                 @author Jean Pierre Charalambos      
  *           http://www.disi.unal.edu.co/grupos/remixlab/
@@ -98,7 +98,7 @@ import java.util.TimerTask;
  */
 public class Scene implements PConstants {
 	// proscene version
-	public static final String version = "1.1.90";
+	public static final String version = "1.1.91";
 	/**
 	 * Returns the major release version number of proscene as an integer.
 	 * <p>
