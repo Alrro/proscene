5f4d283cb67b712b6680c4b9b0d0d26b79890f41
nakednous <nakednous@727c4ca2-1e5a-11df-b3f3-c78161b9101f>
2012-10-23 22:22:05 +0000

    remains to handle resize events in pmpm

--- testing_pmpm_1.5.1/src/basic/Basic.java
+++ testing_pmpm_1.5.1/src/basic/Basic.java
@@ -6,18 +6,33 @@ import remixlab.proscene.*;
 @SuppressWarnings("serial")
 public class Basic extends PApplet {	
 	Scene scene;
-	float d = 150;
-	CameraConstraint constraint;
+	PVector wVec, cVec;
+	PVector screenPos = new PVector(500, 100, 0.5f);
+	InteractiveFrame iFrame;
 
 	public void setup() {
 	  size(640, 360, P3D);
 	  scene = new Scene(this);
+	  scene.setShortcut('f', Scene.KeyboardAction.DRAW_FRAME_SELECTION_HINT);
+	  iFrame = new InteractiveFrame(scene);	  
 	}	
 
 	public void draw() {
 	  background(0);
 	  fill(204, 102, 0);
+	  //box(20, 30, 50);
+	  
+	  wVec = scene.camera().unprojectedCoordinatesOf(screenPos);
+	  //cVec = scene.camera().cameraCoordinatesOf(wVec);
+	  
+	  iFrame.translate(wVec);
+	  
+	  pushMatrix();	 
+	  //fill(104, 20, 130);	  
+	  iFrame.applyTransformation();
+	  scene.drawAxis(20);
 	  box(20, 30, 50);	  
+	  popMatrix();
 	}
 	
 	public static void main(String args[]) {
