e3de28b0896dd639a3e1e60f775a9dd64cea2539
nakednous@gmail.com <nakednous@gmail.com@727c4ca2-1e5a-11df-b3f3-c78161b9101f>
2013-08-09 21:27:09 +0000

    small improvement to the MouseMove example

--- testingproscene2/src/basic/MouseMoveCameraRotate.java
+++ testingproscene2/src/basic/MouseMoveCameraRotate.java
@@ -58,20 +58,16 @@ public class MouseMoveCameraRotate extends PApplet {
 		GenericDOF2Event<Constants.DOF2Action> event, prevEvent;
 		public MouseMoveAgent(AbstractScene scn, String n) {
 			super(scn, n);
-			cameraProfile().setBinding(DOF2Action.ROTATE);
-			cameraProfile().setBinding(TH_LEFT, DOF2Action.TRANSLATE);
+			cameraProfile().setBinding(DOF2Action.ROTATE); // -> MouseEvent.MOVE
+			cameraProfile().setBinding(TH_LEFT, DOF2Action.TRANSLATE); // -> MouseEvent.DRAG
 		}
 		public void mouseEvent(processing.event.MouseEvent e) {
-			if( e.getAction() == processing.event.MouseEvent.MOVE ) {
-				event = new GenericDOF2Event<Constants.DOF2Action>(prevEvent, e.getX() - scene.upperLeftCorner.getX(), e.getY() - scene.upperLeftCorner.getY());
-				handle(event);
-				prevEvent = event.get();
-			}
-			if( e.getAction() == processing.event.MouseEvent.DRAG ) {
+			//don't even necessary :P
+			//if( e.getAction() == processing.event.MouseEvent.MOVE || e.getAction() == processing.event.MouseEvent.DRAG) {
 				event = new GenericDOF2Event<Constants.DOF2Action>(prevEvent, e.getX() - scene.upperLeftCorner.getX(), e.getY() - scene.upperLeftCorner.getY(), e.getModifiers(), e.getButton());
 				handle(event);
 				prevEvent = event.get();
-			}
+			//}
 		}		
 	}
 }
